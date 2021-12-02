f462cbcdceade94be208e0185501f5c9842a7ccb
Erkhemee0908 <erkhemee0908@gmail.com>
2021-12-02 22:50:48 +0800

    Deploy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAWWgbsUV0Bf14mjPfMnIkAmvbscjF8dfA",
+  authDomain: "gamelist-f00b7.firebaseapp.com",
+  projectId: "gamelist-f00b7",
+  storageBucket: "gamelist-f00b7.appspot.com",
+  messagingSenderId: "381261455022",
+  appId: "1:381261455022:web:f1230226174260276e173f",
+  measurementId: "G-SCQ01LKVTY"
+};
+
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
