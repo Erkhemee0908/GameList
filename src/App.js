62cef846cc2f03c0b8276fdf68b0e896ddc1eba8
Erkhembayar Enkhtaivan <erkhemee0908@gmail.com>
2021-12-03 00:40:53 +0800

    Revert "Deployed on Firebase!"

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import './App.css';
 import Games from './components/Games'
 import AppBar from './components/AppBar'
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of 3d8de88 (Deployed on Firebase!)
-=======
->>>>>>> parent of 3d8de88 (Deployed on Firebase!)
-
 function App() {
   return (
     <>
