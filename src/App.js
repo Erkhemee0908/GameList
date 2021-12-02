7096455f8343c60decb9ed92992a12742fee203e
Erkhembayar Enkhtaivan <erkhemee0908@gmail.com>
2021-12-03 00:35:59 +0800

    revert deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Games from './components/Games'
 import AppBar from './components/AppBar'
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 3d8de88 (Deployed on Firebase!)
 =======
 >>>>>>> parent of 3d8de88 (Deployed on Firebase!)
 
